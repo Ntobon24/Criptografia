47efa0acbb9da93b96056647d7182aa6d1c3bb71
Nicolas Tobon <nicolastobon242>
2023-05-07 18:25:55 -0500

    encriptar listo

--- criptografia.py
+++ criptografia.py
@@ -11,29 +11,36 @@ b = random.randint(1, 100000000000000)
 
 
 def encriptar_mensaje(mensaje, a, b):
+    global mensaje_hex
     mensaje_encriptado = ""
     for caracter in mensaje:
         indice = caracteres.index(caracter)
         indice_encriptado = (a * indice + b) % 104
         caracter_encriptado = caracteres[indice_encriptado]
         mensaje_encriptado += caracter_encriptado
+        mensaje_hex = mensaje_encriptado.encode("utf-8").hex()
 
-    forma = random.randint(1, 3)
+    forma = random.randint(1, 6)
     if forma == 1:
-        mensaje_final = f"{mensaje_encriptado}&{a}&{b}"
+        mensaje_final = f"{a}&{b}&{mensaje_hex}"
     elif forma == 2:
-        mensaje_final = f"{a}&{b}&{mensaje_encriptado}"
+        mensaje_final = f"{a}&{mensaje_hex}&{b}"
+    elif forma == 3:
+        mensaje_final = f"{b}&{a}&{mensaje_hex}"
+    elif forma == 4:
+        mensaje_final = f"{b}&{mensaje_hex}&{a}"
+    elif forma == 5:
+        mensaje_final = f"{mensaje_hex}&{a}&{b}"
     else:
-        mensaje_final = f"{a}&{mensaje_encriptado}&{b}"
+        mensaje_final = f"{mensaje_hex}&{b}&{a}"
 
     return mensaje_final
 
 
 mensaje = input("Ingrese el mensaje a encriptar: ")
 
-mensaje_encriptado = encriptar_mensaje(mensaje, a, b)
+mensaje_final = encriptar_mensaje(mensaje, a, b)
 
-mensaje_hex = mensaje_encriptado.encode("utf-8").hex()
-
-print("Mensaje encriptado en hexadecimal: " + mensaje_hex)
-
+print(f"Mensaje encriptado: {mensaje_final}")
+print(f"random a: {a}")
+print(f"random b: {b}")
